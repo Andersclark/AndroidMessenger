4d218455a069edb697d98eb00625aa314b4fe668
Anders Clark <anders.clark@gmail.com>
2020-01-10 16:25:57 +0100

    Added Registration-functionality

--- app/src/main/java/com/andersclark/kotlinmessenger/MainActivity.kt
+++ app/src/main/java/com/andersclark/kotlinmessenger/MainActivity.kt
@@ -17,14 +17,17 @@ class MainActivity : AppCompatActivity() {
 
             val email = registration_email.text.toString()
             val password = registration_password.text.toString()
-            Log.d("MainActivity", "EMAIL: $email")
-            Log.d("MainActivity", "PASSWORD: $password")
-
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener{
+                   if (!it.isSuccessful)return@addOnCompleteListener
+                    // else if successful
+                    Log.d("MainActivity", "SUCCESSFULLY CREATED USER with UID: ${it.result?.user?.uid}")
+                }
         }
         registration_alreadyhaveaccount.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
            startActivity(intent)
         }
-        FirebaseAuth.getInstance()
+
     }
 }
