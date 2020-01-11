7faadae2887f5187bd1f55358d67d789d224805a
Andersclark <anders.clark@gmail.com>
2020-01-11 14:00:55 +0100

    Login now works. Added UPLOAD PHOTO-button (no logic yet)

--- app/src/main/java/com/andersclark/kotlinmessenger/LoginActivity.kt
+++ app/src/main/java/com/andersclark/kotlinmessenger/LoginActivity.kt
@@ -3,8 +3,10 @@ package com.andersclark.kotlinmessenger
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.content_login.*
@@ -21,6 +23,16 @@ class LoginActivity : AppCompatActivity() {
             val password = login_password.text.toString()
             Log.d("LoginActivity", "EMAIL: $email")
             Log.d("LoginActivity", "PASSWORD: $password")
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    Log.d("LoginActivity", "Login complete")
+                    if (!it.isSuccessful) return@addOnCompleteListener
+                }
+                .addOnFailureListener {
+                    Log.d("LoginActivity", "Failed to log in: ${it.message}")
+                    Toast.makeText(this, "Failed to log in: ${it.message}", Toast.LENGTH_SHORT)
+                        .show()
+                }
         }
         login_returntoregistration.setOnClickListener {
             finish()
--- app/src/main/java/com/andersclark/kotlinmessenger/MainActivity.kt
+++ app/src/main/java/com/andersclark/kotlinmessenger/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,21 +14,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        registration_registerbutton.setOnClickListener{
-
-            val email = registration_email.text.toString()
-            val password = registration_password.text.toString()
-            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener{
-                   if (!it.isSuccessful)return@addOnCompleteListener
-                    // else if successful
-                    Log.d("MainActivity", "SUCCESSFULLY CREATED USER with UID: ${it.result?.user?.uid}")
-                }
+        registration_registerbutton.setOnClickListener {
+            performRegister()
         }
         registration_alreadyhaveaccount.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
-           startActivity(intent)
+            startActivity(intent)
         }
+    }
 
+    private fun performRegister() {
+
+        val email = registration_email.text.toString()
+        val password = registration_password.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter a viable email and password", Toast.LENGTH_SHORT)
+                .show()
+            return
+        }
+        Log.d("MainActivity", "Email is: ${email}")
+        Log.d("MainActivity", "Password is: ${password}")
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+                // else if successful
+                Log.d(
+                    "MainActivity",
+                    "SUCCESSFULLY CREATED USER with UID: ${it.result!!.user?.uid}"
+                )
+            }
+            .addOnFailureListener {
+                Log.d("MainActivity", "Failed to create user: ${it.message}")
+                Toast.makeText(this, "Failed to create user: ${it.message}", Toast.LENGTH_SHORT)
+                    .show()
+            }
     }
 }
